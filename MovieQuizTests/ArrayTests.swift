7bb01947ad0668dece20e6a56464179eb50bef86
Avonic <boysfromhell@mail.ru>
2025-02-15 09:09:43 +0300

    исправление ошибок с ревью

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -21,7 +21,7 @@ class ArrayTests: XCTestCase {
         //given
         let array = [1,2,3,4,5]
         //When
-        let value = array[safe:2]
+        let value = array[safe:20]
         //Then
         XCTAssertNil(value)
     }
